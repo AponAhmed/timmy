0898c1215fa5d5391677f8b267adcf0a980beea4
MD. Muhiminul Haque <apon2041@gmail.com>
2024-10-15 16:24:33 +0600

    Screen size updated

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-import './style.css'
-import Timmy from './src/Timmy'
+import './style.css';
+import Timmy from './src/Timmy';
 
-const timmy = new Timmy(800, 400);
+// Function to get the width and height based on the device size
+const getDeviceDimensions = () => {
+    const isSmallDevice = window.innerWidth < 800; // Check if the device width is less than 800
+    const width = isSmallDevice ? window.innerWidth : 800; // Set width to window width if small device
+    const height = width / 2; // Maintain a 2:1 aspect ratio
+    return { width, height };
+};
+
+const { width, height } = getDeviceDimensions(); // Get the dimensions based on device size
+
+const timmy = new Timmy(width, height);
 timmy.init();
-
